2c701581393214b3f4a20e07269b268ee7a340a3
mBerdis <marosberdis@gmail.com>
2020-12-03 10:40:48 +0100

    Uloha1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello Matej");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("JavaFX experimenty");
+        primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
     }
 
