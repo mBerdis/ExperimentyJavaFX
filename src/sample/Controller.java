b6b6287cdddecc56415d3bfcca7ea63e85c74d67
mihalcinD <mihalcindavid@gmail.com>
2020-12-05 18:59:03 +0100

    progress

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+
 public class Controller {
+
+    public Button zvacsiBtn,zmensiBtn;
+    public Label labelAhoj;
+    public void zmensiButtony()
+    {
+        labelAhoj.setFont(new Font(+20));
+
+    }
+
+    public void zvacsiButtony()
+    {
+        System.out.println(labelAhoj.getFont());
+        labelAhoj.setFont(new Font(+40));
+
+    }
+
+
 }
