75baa7aa4911f9b95c85ef2597d6a4b40b595fe5
mihalcinD <mihalcindavid@gmail.com>
2020-12-05 20:15:07 +0100

    Ulohy 0-4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,16 @@
 package sample;
-
-
-import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.paint.*;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 
-public class Controller
-{
-    public Button zvacsiBtn,zmensiBtn;
+public class Controller {
+
     public Label labelAhoj;
+    public Circle circle;
+    public Rectangle rectangle;
 
     public void decrease_font_size()
     {
@@ -19,4 +21,22 @@ public class Controller
         labelAhoj.setFont(new Font(labelAhoj.getFont().getSize() + 20));
     }
 
+
+    public void circle_to_left()
+    {
+        circle.setCenterX(circle.getCenterX()-20);
+
+    }
+
+    public void circle_to_right()
+    {
+        circle.setCenterX(circle.getCenterX()+20);
+    }
+
+    public void rectangle_rainbow()
+    {
+        Stop[] stops = new Stop[] { new Stop(0, Color.RED), new Stop(1, Color.YELLOW)};
+        LinearGradient linear = new LinearGradient(0, 0, 0, 1, true, CycleMethod.NO_CYCLE, stops);
+        rectangle.setFill(linear);
+    }
 }
